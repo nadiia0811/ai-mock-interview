be75fcae020d6220d07844507d35e46f1c4720d6
Nadya <nb1985@ukr.net>
2025-06-03 18:26:51 +0300

    feat(components): add AuthForm component supporting sign-in and sign-up states

---
+++ components/AuthForm.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import React from 'react';
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form";
+import FormField from "@/components/FormField";
+import Image from 'next/image';
+import Link from 'next/link';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+  })
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+    const router = useRouter();
+    const formSchema = authFormSchema(type);
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+      try {
+        if (type === "sign-up") {
+            toast.success("Account created successfully. Please Sign in");
+            router.push("/sign-in");
+        } else {
+            toast.success("Sign in successfully");
+            router.push("/");
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error(`The error occured: ${error}`);
+      }
+    };
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: ""
+        },
+    });
+    
+    const isSignIn = type === "sign-in";
+    return (  
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </div>
+
+                <h3 className="flex justify-center">Practice job interview with AI</h3>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} 
+                          className="space-y-6 w-full mt-4 form">
+                        { !isSignIn &&  <FormField name="name"
+                                                   label="Name"
+                                                   control={form.control} 
+                                                   placeholder="Your Name"
+                                        />
+                        }
+                        <FormField name="email"
+                                   label="Email"
+                                   control={form.control}
+                                   type="email" 
+                                   placeholder="Email"
+                        />
+                        <FormField name="password"
+                                   type="password"
+                                   label="Password"
+                                   control={form.control} 
+                                   placeholder="Password"
+                        />
+                        <Button type="submit"
+                                className="btn">
+                            { isSignIn ? "Sign In" : "Create an Account" }
+                        </Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    { isSignIn ? "Don't have an account? " : "Already have an account?"}
+                    <Link href={ isSignIn ? "/sign-up" : "/sign-in" }
+                          className="font-bold text-orange-200 ml-4">
+                       { isSignIn ? "Create an Account" : "Sign In" }
+                    </Link>
+                </p>
+            </div>
+        </div>
+    ) 
+}
+
+export default AuthForm;
