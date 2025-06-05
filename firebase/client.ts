4ef63cf4c17ecc813297d449cfdb7ee432f3b859
Nadya <nb1985@ukr.net>
2025-06-05 10:08:46 +0300

    feat: configure Firebase client SDK with auth and Firestore

--- firebase/client.ts
+++ firebase/client.ts
@@ -14,4 +14,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
