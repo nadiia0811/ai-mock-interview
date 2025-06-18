7d70f90e28f3590767b30011f26be650898fe15d
Nadya <nb1985@ukr.net>
2025-06-18 19:20:04 +0300

    feat: show current user's interviews on main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,50 +1,62 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import Image from 'next/image';
-import React from 'react';
-import { dummyInterviews } from '@/constants';
-import InterviewCard from '@/components/InterviewCard';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
+import { getInterviewsByUserId, getCurrentUser } from "@/lib/actions/auth.action";
 
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+  const userInterviews = await getInterviewsByUserId(user?.id!);
+  const hasPastInterviews = userInterviews.length > 0;
+
   return (
     <>
       <section className="card-cta">
-         <div className="flex flex-col gap-6 max-w-lg">
-            <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-            <p className="text-lg">
-               Practice on real interview questions & get instant feedback
-            </p>
-            <Button asChild
-                    className="btn-primary max-sm:w-full">
-              <Link href="/interview">Start an Interview</Link>
-            </Button>
-         </div>
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
 
-         <Image src="/robot.png" alt="robot" width={400} height={400}
-                className="max-sm:hidden" />
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-           { dummyInterviews.map((interview) => (
-              <InterviewCard key={interview.id} {...interview} />
-           ))}
-          {/* <p>You haven&apos;t taken any interview yet</p> */}
+          {hasPastInterviews
+            ? userInterviews?.map((interview) => (
+                <InterviewCard key={interview.id} {...interview} />
+              ))
+            : dummyInterviews.map((interview) => (
+                <InterviewCard key={interview.id} {...interview} />
+              ))}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
-         <h2>Take an Interview</h2>
-         <div className="interviews-section">
-          { dummyInterviews.map((interview) => (
-                <InterviewCard key={interview.id} {...interview} />
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
           ))}
-         </div>
-      </section>
+        </div>
+      </section> 
     </>
-  )
+  );
 };
 
 export default Page;
