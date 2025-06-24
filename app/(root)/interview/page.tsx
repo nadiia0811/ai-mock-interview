9453ea769c1c843ac44203852c108a9a4c6b4bff
Nadya <nb1985@ukr.net>
2025-06-24 13:39:15 +0300

    chore: clean up dummy interviews on interview page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -2,7 +2,6 @@ export const runtime = "nodejs";
 
 import Agent from '@/components/Agent';
 import { getCurrentUser } from '@/lib/actions/auth.action';
-import React from 'react';
 
 const page = async () => {
   const user = await getCurrentUser();
